a54c3dfb857adb1b146ac45589cedf5160b1495d
AppSupportAPI <schulz.kilian@outlook.de>
2024-01-21 12:15:13 +0100

    added functionality to delete a photo

--- service/api/deletePhoto.go
+++ service/api/deletePhoto.go
@@ -1,14 +1,32 @@
 package api
 
 import (
-    "fmt"
     "net/http"
+    "strconv"
     "github.com/julienschmidt/httprouter"
     "git.sapienzaapps.it/fantasticcoffee/fantastic-coffee-decaffeinated/service/api/reqcontext" // replace with your actual package import path
 )
 
 func deletePhoto(w http.ResponseWriter, r *http.Request, ps httprouter.Params, ctx reqcontext.RequestContext) {
     // Placeholder logic
-    ctx.Logger.Info("myApiHandler called") // Example logging
-    fmt.Fprintf(w, "This is a placeholder for myApiHandler")
+    photo_id := ps.ByName("photoId")
+    ctx.Logger.Info(photo_id)
+    db := ctx.Database
+    photoid, err := strconv.Atoi(photo_id)
+    if err != nil {
+        // If an error occurs, send a 404 Not Found error
+        http.Error(w, "Type mismatch", http.StatusBadRequest)
+        return
+    }
+    err = db.DeletePhoto(photoid)
+    if err != nil {
+        // If an error occurs, send a 404 Not Found error
+        http.Error(w, "Photo not found", http.StatusNotFound)
+        return
+    }
+
+    // If no error, send a success response
+    w.WriteHeader(http.StatusOK)
+    w.Write([]byte("Photo deleted successfully"))
+
 }
