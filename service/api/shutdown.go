e74dbd386e00c52e15101d029364054c54cdd6be
AppSupportAPI <schulz.kilian@outlook.de>
2024-01-24 10:26:41 +0100

    adding server error as possible response

--- service/api/shutdown.go
+++ service/api/shutdown.go
@@ -1,11 +1,11 @@
 package api
 
 func (rt *_router) Close() error {
-	if rt.db != nil {
-		if err := rt.db.Close(); err != nil {
-			return err // return error if db.Close() fails
-		}
-	}
-	// ... close other resources ...
-	return nil
+    if rt.db != nil {
+        if err := rt.db.Close(); err != nil {
+            return err // return error if db.Close() fails
+        }
+    }
+
+    return nil
 }
